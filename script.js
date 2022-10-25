18c48439e56b7fcc3820e6995d933c1916b259b8
Kyle Merrill <kylemerrill18@gmail.com>
2022-10-24 21:40:24 -0400

    Reference JS file. Create random output function

---
+++ script.js
@@ -0,0 +1,12 @@
+function getComputerChoice() {
+    randomNum = Math.floor(Math.random() * 4);
+    if (randomNum === 1) {
+        console.log('Rock');
+    } else if (randomNum == 2) {
+        console.log('Paper');
+    } else {
+        console.log('Scissors');
+    }
+}
+
+getComputerChoice();
