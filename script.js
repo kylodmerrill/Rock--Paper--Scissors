39854ef9ff4588dc57ca8e684677573d9fc9fce5
Kyle Merrill <kylemerrill18@gmail.com>
2022-10-24 22:15:15 -0400

    Write playRound function. Modify getComputerChoice function to return

--- script.js
+++ script.js
@@ -1,10 +1,31 @@
 function getComputerChoice() {
     randomNum = Math.floor(Math.random() * 4);
-    if (randomNum === 1) {
-        console.log('Rock');
+    if (randomNum == 1) {
+        return('Rock');
     } else if (randomNum == 2) {
-        console.log('Paper');
+        return('Paper');
     } else {
-        console.log('Scissors');
+        return('Scissors');
     }
 }
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+    if (playerSelection == 'rock' && computerSelection == 'scissors') {
+        return 'Rock beats Scissors. You win!';
+    } else if (playerSelection == 'paper' && computerSelection == 'rock') {
+        return 'Paper beats Rock. You win!';
+    } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
+        return 'Scissors beats Paper. You win!';
+    } else if (playerSelection == 'rock' && computerSelection == 'paper') {
+        return 'Paper beats Rock. You lose!';
+    } else if (playerSelection == 'paper' && computerSelection == 'scissors') {
+        return 'Scissors beats Paper. You lose!';
+    } else if (playerSelection == 'scissors' && computerSelection == 'rock') {
+        return 'Rock beats Scissors. You lose!';
+    } else {
+        return 'Tie!';
+    }
+}
+
